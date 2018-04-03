746c87ebff371bff2ee15b541495161a1f7781f7
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-03 00:21:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,10 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     return false
-  elsif !index.between? (0,8)
+  elsif !index.between?(0,8)
     return false
   end
-  
+
   return true
 end
 

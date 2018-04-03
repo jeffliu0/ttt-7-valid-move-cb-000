6f03ac8ba9ef6b80148cd1d4c9c1cf9361b1ce89
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-03 00:15:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,10 @@
 def valid_move?
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  flag = [" ","", nil]
+  if flag.include? board[index]
+    return false
+  end
+  return true
+end
